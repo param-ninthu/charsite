d98c9c66f8d1e36e341040b8ed895cbfb0bde3f7
chosen <67898034+param-ninthu@users.noreply.github.com>
2023-08-07 12:44:42 +0530

    nav bar layout finished

--- src/components/NavBar/Nav.jsx
+++ src/components/NavBar/Nav.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Nav.css";
+
 const Nav = () => {
   return (
     <div className="nav-container">
       <div className="nav">
         <div className="nav-items">Logo</div>
-        <div className="nav-items"> Link </div>
+        <div className="nav-items">
+          {" "}
+          <ul className="nav-items ">
+            <li className="list"> Home </li>
+            <li className="list"> Aboutus </li>
+            <li className="list"> Gallery </li>
+            <li className="list donate-button"> Donate </li>
+          </ul>{" "}
+        </div>
       </div>
     </div>
   );
